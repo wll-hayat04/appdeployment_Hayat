969ceabb7758427f6dccaee621f4020347a83918
wll-hayat04 <waldihayat@gmail.com>
2025-11-29 21:33:42 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Configuration de la page
+
+st.set_page_config(
+    page_title="Prédiction du risque de CHD",
+    page_icon="🫀",
+    layout="centered"
+)
